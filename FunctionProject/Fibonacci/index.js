f99fbce97d53891d398e1d6a8482899d82435ae9
Santiago Buitrago <brayan.buitrago@escuelaing.edu.co>
2021-04-27 22:08:47 -0500

    Imagenes

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -1,25 +1,41 @@
+/**
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * 													CLASE: index
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * Fibonacci App
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * @author Santiago Buitrago
+ * @version 1.0
+ * ---------------------------------------------------------------------------------------------------------------------------
+ */
 var bigInt = require("big-integer");
+var memorizacion = {};
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
+	
+	memorizacion[0] = bigInt.zero;
+    memorizacion[1] = bigInt.one;
     let nth = req.body.nth
     let nth_1 = bigInt.one;
     let nth_2 = bigInt.zero;
     let answer = bigInt.zero;
-
-    if (nth < 0)
-        throw 'must be greater than 0'
-    else if (nth === 0)
-        answer = nth_2
-    else if (nth === 1)
-        answer = nth_1
-    else {
-        for (var i = 0; i < nth - 1; i++) {
-            answer = nth_2.add(nth_1)
-            nth_2 = nth_1
-            nth_1 = answer
+	
+	function fibonacci(n) {
+		var a = bigInt.zero
+        var b = bigInt.zero
+        n = Math.abs(n);
+		if(memorizacion[n] !== undefined)
+			return memorizacion[n];
+		else{
+           a=fibonacci(n-1)
+           b =fibonacci(n-2)
+           memorizacion[n] = a.add(b)
+           return memorizacion[n];
         }
-    }
+	}
+	
+    answer = fibonacci(nth);
 
     context.res = {
         body: answer.toString()
